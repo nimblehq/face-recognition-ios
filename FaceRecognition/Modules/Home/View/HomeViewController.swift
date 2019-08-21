d8798644b158297b3a96795ecc0de660919521ef
Su Ho <su@nimblehq.co>
2019-08-21 11:24:41 +0700

    Add TableView

--- FaceRecognition/Modules/Home/View/HomeViewController.swift
+++ FaceRecognition/Modules/Home/View/HomeViewController.swift
@@ -20,6 +20,7 @@ protocol HomeViewOutput: AnyObject {
 
 final class HomeViewController: UIViewController {
 
+    let tableView = UITableView()
     var output: HomeViewOutput?
 
     override func viewDidLoad() {
@@ -31,7 +32,42 @@ final class HomeViewController: UIViewController {
 // MARK: - HomeViewInput
 extension HomeViewController: HomeViewInput {
     func configure() {
+        setUpLayouts()
+        setUpViews()
+    }
+
+    private func setUpLayouts() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { $0.edges.equalToSuperview() }
+    }
+
+    private func setUpViews() {
         navigationItem.title = "Face Recognition"
         view.backgroundColor = .white
+        setUpTableView()
+    }
+
+    private func setUpTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: String(describing: UITableViewCell.self))
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
     }
 }
+
+// MARK: - UITableViewDataSource
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: UITableViewCell.self), for: indexPath)
+        cell.textLabel?.text = "Take picture for training model"
+        cell.selectionStyle = .none
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension HomeViewController: UITableViewDelegate {}
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FaceRecognition' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FaceRecognition
+  pod 'SnapKit'
+
+  target 'FaceRecognitionTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'FaceRecognitionUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
